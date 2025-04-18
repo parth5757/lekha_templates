5309a075b74537b7766fd2b5d560fa38127567be
Parth Thakkar <psthakkar2@gmail.com>
2025-04-18 18:05:13 +0530

    peoject colr variable file added.

---
+++ Theme_1-Web/lekha/src/utils/colors.js
@@ -0,0 +1,20 @@
+const lightTheme = {
+    background: '#FFFFFF',
+    secondary: '#D6D6D6',
+    primary: '#00D272',
+    text: '#1F1F1F',
+};
+
+const darkTheme = {
+    background: '#121212',
+    secondary: '#D6D6D6',
+    primary: '#00D272',
+    text: '#1F1F1F',
+};
+
+const themes = {
+    light: lightTheme,
+    dark: darkTheme,
+}
+
+export default themes;
